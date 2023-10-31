2ca7535978ca67b41636576d0b86d3c82b57fb0a
Anonymous <86862812+HALPINGHACKER@users.noreply.github.com>
2023-10-31 23:02:14 +0530

    Add files via upload

---
+++ livehost.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Usage: ./find_subdomains.sh <domain>
+# Example: ./find_subdomains.sh example.com
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <domain>"
+    exit 1
+fi
+
+domain="$1"
+
+# Wordlist containing potential subdomains to check
+wordlist="subdomains.txt"
+
+# Function to check if a subdomain is live
+check_subdomain() {
+    subdomain="$1.$domain"
+    if nslookup "$subdomain" &> /dev/null; then
+        echo "$subdomain is live"
+    fi
+}
+
+# Loop through the wordlist and check each subdomain
+while IFS= read -r line; do
+    check_subdomain "$line"
+done < "$wordlist"
+
